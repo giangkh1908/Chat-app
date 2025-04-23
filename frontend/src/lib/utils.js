bd8eb69d451f9a1ccf81f5438345664501134914
Kim Hong Giang <giangkh1908@gmail.com>
2025-04-23 21:54:48 +0700

    test

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,5 +1,5 @@
 export function formatMessageTime(date) {
-  return new Date(date).toLocaleTimeString("en-US", {
+  return new Date(date).toLocaleTimeString("VN-vn", {
     hour: "2-digit",
     minute: "2-digit",
     hour12: false,
